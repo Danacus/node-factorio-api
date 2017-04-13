e365182107088129360fd0af789bcfbdfba672de
Daan Vanoverloop <vanoverloopdaan@gmail.com>
2017-04-13 14:59:15 +0200

    Updated documentation

--- src/node-factorio-api.js
+++ src/node-factorio-api.js
@@ -11,8 +11,8 @@ class FactorioAPI {
  * This function initializes the api.
  * @param {boolean} [allowMultipleVersions=false]
  * @param {string} [modPath=''] path to the mods folder
- * @param {string} [savePath=''] path to the saves folder (optional)
- * @param {string} [gameVersion='0.0.0'] the version of the game (optional)
+ * @param {string} [savePath=''] path to the saves folder
+ * @param {string} [gameVersion='0.0.0'] the version of the game
  */
   static init(allowMultipleVersions = false, modPath = '', savePath = '', gameVersion = "0.0.0") {
     this.token = null
@@ -41,31 +41,53 @@ class FactorioAPI {
   }
 
   /**
- * This function sets the mod path.
+ * This function sets the save path.
  * @param {string} savePath path to the saves folder
  */
   static setSavePath(savePath) {
     this.savePath = savePath
   }
 
+  /**
+ * This function gets the save path.
+ * @returns {string} path to the saves folder
+ */
   static getSavePath() {
     return this.savePath
   }
 
+  /**
+ * This function gets the mod path.
+ * @returns {string} path to the mods folder
+ */
   static getModPath() {
     return this.modPath
   }
 
+  /**
+  * This function gets the game version.
+  * @returns {string} the version of the game
+  */
   static getGameVersion() {
     return this.gameVersion
   }
 
+  /**
+  * This function returns true if the api is authenticated
+  * @returns {boolean} authenticated
+  */
   static isAuthenticated() {
     return this.authenticated
   }
 
-  // ------------------------
-  // Authenication with username and password/token
+  /**
+ * This function authenticates with a username and password or token
+ * @param {Object} props the properties for authentication
+ * @param {string} props.username the username
+ * @param {string} props.password not recommended, use token instead
+ * @param {string} props.token the player token, can be found in player-data.json
+ * @returns {Promise.<string>} returns token if resolved
+ */
   static authenticate(props) {
     return new Promise((resolve, reject) => {
       props.require_ownership = typeof props.require_ownership !== 'undefined' ? props.require_ownership : false
@@ -99,9 +121,12 @@ class FactorioAPI {
     })
   }
 
-  // ------------------------
-  // Mod Portal
-  // More information: https://wiki.factorio.com/Mod_Portal_API
+  /**
+ * This function gets the full information of a mod
+ * @param {string} name the name of the mod
+ * @returns {Promise.<Object>} returns the information about the mod if resolved
+ * @see {@link https://wiki.factorio.com/Mod_Portal_API#Result_Entry|Factorio Wiki}
+ */
   static getMod(name) {
     return new Promise((resolve, reject) => {
       let options = {
@@ -118,11 +143,26 @@ class FactorioAPI {
     })
   }
 
+  /**
+ * This function gets the full information of multiple mods
+ * @param {Object[]} mods the mods
+ * @param {string} mods[].name the name of the mod
+ * @returns {Promise.<Object[]>} returns array of mods with the information about each mod if resolved
+ * @see {@link https://wiki.factorio.com/Mod_Portal_API#Result_Entry|Factorio Wiki}
+ */
   static getMods(mods) {
     let promises = mods.map(mod => getMod(mod.name))
     return Promise.all(promises)
   }
 
+  /**
+  * This function searches for mods on the Mod Portal
+  * @param {Object} props the search properties, some examples below, there might be more options
+  * @param {string} props.q a search query
+  * @param {string} props.order the order of mods, possible values: top, alpha, updated
+  * @param {string} props.page_size the size of a page
+  * @returns {Promise.<Object>} returns the result if resolved (here's an example: https://mods.factorio.com/api/mods?q=FARL)
+  */
   static searchMods(props) {
     return new Promise((resolve, reject) => {
       let options = {
@@ -140,19 +180,14 @@ class FactorioAPI {
     })
   }
 
-  // ------------------------
-  // mods = [{name: {name}, version: {current version}}]
-  static updateMods(mods, factorioVersion = this.gameVersion) {
-    let promises = mods.map(mod => this.updateMod(mod, factorioVersion))
-    return Promise.all(promises)
-  }
-
-  // ------------------------
-  /*
-  mod = {
-    name: {name},
-    version: {current version}
-  }
+  /**
+  * This function downloads the latest version of a mod, compatible with the given game version, if there's a more recent version available
+  * @param {Object} mod the mod you want to update
+  * @param {string} mod.name the name of the mod
+  * @param {string} mod.version the version you have installed
+  * @param {string} [factorioVersion=this.gameVersion] the version of the game
+  * @returns {Promise.<Object>} returns the information about the mod if resolved
+  * @see {@link https://wiki.factorio.com/Mod_Portal_API#Result_Entry|Factorio Wiki}
   */
   static updateMod(mod, factorioVersion = this.gameVersion) {
     return new Promise((resolve, reject) => {
@@ -172,6 +207,28 @@ class FactorioAPI {
     })
   }
 
+  /**
+  * This function updates all given mods and resolves if all mods are downloaded
+  * @param {Object[]} mods the mods you want to update
+  * @param {string} mod[].name the name of the mod
+  * @param {string} mod[].version the version you have installed
+  * @param {string} [factorioVersion=this.gameVersion] the version of the game
+  * @returns {Promise.<Object>} returns the information about each mod if resolved
+  * @see {@link https://wiki.factorio.com/Mod_Portal_API#Result_Entry|Factorio Wiki}
+  */
+  static updateMods(mods, factorioVersion = this.gameVersion) {
+    let promises = mods.map(mod => this.updateMod(mod, factorioVersion))
+    return Promise.all(promises)
+  }
+
+  /**
+  * This function checks for updates for a mod, compatible with the given game version
+  * @param {Object} mod the mod you want to update
+  * @param {string} mod.name the name of the mod
+  * @param {string} mod.version the version you have installed
+  * @param {string} [factorioVersion=this.gameVersion] the version of the game
+  * @returns {Promise.<Object>} returns {onlineMod, hasUpdate: true, version: release.version} if an update is available and returns {onlineMod, hasUpdate: false} when the mod is up-to-date
+  */
   static checkUpdate(mod, factorioVersion = this.gameVersion) {
     return new Promise((resolve, reject) => {
       this.getMod(mod.name).then((onlineMod) => {
@@ -195,24 +252,27 @@ class FactorioAPI {
     })
   }
 
+  /**
+  * This function checks for updates for all given mods
+  * @param {Object[]} mods the mods you want to update
+  * @param {string} mods[].name the name of the mod
+  * @param {string} mods[].version the version you have installed
+  * @param {string} [factorioVersion=this.gameVersion] the version of the game
+  * @returns {Promise.<Object[]>} returns an array of all objects returned by checkUpdate
+  */
   static checkUpdates(mods, factorioVersion = this.gameVersion) {
     let promises = mods.map((mod) => this.checkUpdate(mod, factorioVersion))
     return Promise.all(promises)
   }
 
-  // ------------------------
-  // mods = [{name: {name}, version: {version you want to download}}]
-  static downloadMods(mods) {
-    let promises = mods.map(mod => this.downloadMod(mod))
-    return Promise.all(promises)
-  }
-
-  // ------------------------
-  /*
-  mod = {
-    name: {name},
-    version: {version you want to download}
-  }
+  /**
+  * This function downloads a mod
+  * @param {Object} mod the mod you want to download
+  * @param {string} mod.name the name of the mod
+  * @param {string} mod.version the version you want to download (optional, default is latest)
+  * @param {string} [factorioVersion=this.gameVersion] the version of the game
+  * @returns {Promise.<Object>} returns the online mod information if resolved
+  * @see {@link https://wiki.factorio.com/Mod_Portal_API#Result_Entry|Factorio Wiki}
   */
   static downloadMod(mod) {
     return new Promise((resolve, reject) => {
@@ -236,15 +296,31 @@ class FactorioAPI {
     })
   }
 
-  // ------------------------
-  // url = {download_url property from release}
-  // (example: /api/downloads/data/mods/id/name_version.zip)
+  /**
+  * This function downloads mods
+  * @param {Object} mods the mods you want to download
+  * @param {string} mods[].name the name of the mod
+  * @param {string} mods[].version the version you want to download (optional, default is latest)
+  * @param {string} [factorioVersion=this.gameVersion] the version of the game
+  * @returns {Promise.<Object[]>} returns array of online mods if resolved
+  * @see {@link https://wiki.factorio.com/Mod_Portal_API#Result_Entry|Factorio Wiki}
+  */
+  static downloadMods(mods) {
+    let promises = mods.map(mod => this.downloadMod(mod))
+    return Promise.all(promises)
+  }
+
+  /**
+  * This function downloads a mod from a url
+  * @param {string} url the download_url of a release (example: /api/downloads/data/mods/id/name_version.zip)
+  * @returns {Promise.<string>} returns name of the mod if resolved
+  * @see {@link https://wiki.factorio.com/Mod_Portal_API#Releases|Factorio Wiki}
+  */
   static downloadModFromUrl(url) {
     return new Promise((resolve, reject) => {
       let fullUrl = 'https://mods.factorio.com' + url + `?username=${this.username}&token=${this.token}`
       let fileName = url.substr(url.lastIndexOf('/') + 1);
       let name = fileName.replace(fileName.substr(fileName.lastIndexOf('_')), '')
-
 
       progress(request(fullUrl))
       .on('error', (err) => {
@@ -275,6 +351,13 @@ class FactorioAPI {
     })
   }
 
+  /**
+  * This function removes all mod zip files matching a glob pattern (relative to mod path)
+  * @param {Object} mod the mod you want to remove
+  * @param {string} mod.name the name of the mod, can be a glob pattern
+  * @param {string} mod.version the version you want to remove, can be a glob pattern too (optional, if not specified, all versions will be removed)
+  * @returns {Promise} resolves if successful
+  */
   static removeModsMatching(mod) {
     return new Promise((resolve, reject) => {
       let promises = []
@@ -291,14 +374,30 @@ class FactorioAPI {
 
       Promise.all(promises).then(() => {
         resolve()
+      }).catch((err) => {
+        reject(err)
       })
     })
   }
 
+  /**
+  * This function removes all mod zip files from the mods in the array
+  * @param {Object[]} mods the mods you want to remove
+  * @param {string} mod[].name the name of the mod, can be a glob pattern
+  * @param {string} mod[].version the version you want to remove, can be a glob pattern too (optional, if not specified, all versions will be removed)
+  * @returns {Promise} resolves if successful
+  */
   static removeMods(mods) {
     return Promise.all(mods.map(mod => this.removeModsMatching(mod)))
   }
 
+  /**
+  * This function downloads all dependencies for a mod
+  * @param {Object} mod the mod
+  * @param {string} mod.name the name of the mod
+  * @param {boolean} [optionalMods=false] set to true if you want to download all optional mods (I don't know why you would ever want to do that, but you can!)
+  * @returns {Promise.<Object>} returns all online mods if resolved
+  */
   static downloadDependencies(mod, optionalMods = false) {
     return new Promise((resolve, reject) => {
       this.getDependencies(mod, optionalMods).then((dependencies) => {
@@ -311,6 +410,13 @@ class FactorioAPI {
     })
   }
 
+  /**
+  * This function gets all dependencies for a mod
+  * @param {Object} mod the mod
+  * @param {string} mod.name the name of the mod
+  * @param {boolean} [optionalMods=false] set to true if you want to download all optional mods (I don't know why you would ever want to do that, but you can!)
+  * @returns {Promise.<Object[]>} returns array of mods with their name as property ([{name: name_of_the_mod}]) if resolved
+  */
   static getDependencies(mod, optionalMods = false) {
     return new Promise((resolve, reject) => {
       this.getMod(mod.name).then((onlineMod) => {
@@ -350,9 +456,10 @@ class FactorioAPI {
     })
   }
 
-  // ------------------------
-  // Matchmaking
-  // More information: https://wiki.factorio.com/Matchmaking_API
+  /**
+  * This function gets all available online games
+  * @returns {Promise.<Object>} returns result of the request (https://multiplayer.factorio.com/get-games?username=<your_username>&token=<your_token>)
+  */
   static getGames() {
     let options = {
         method: 'GET',
@@ -367,8 +474,11 @@ class FactorioAPI {
     return rp(options)
   }
 
-  // Get more details from a game
-  // gameId can be retrieved from getGames()
+  /**
+  * This function gets details of a game
+  * @param {string} gameId the id of an online game (get it from getGames())
+  * @returns {Promise.<Object>} returns result of the request (https://multiplayer.factorio.com/get-games?username=<your_username>&token=<your_token>)
+  */
   static getGameDetails(gameId) {
     let options = {
         method: 'GET',
@@ -379,8 +489,13 @@ class FactorioAPI {
     return rp(options)
   }
 
-  // Get mods from level-init.dat (in save zip archive)
-  static getModsFromSaveFile(fileName, resolveFileName = false) {
+  /**
+  * This function gets mods from a save file
+  * @param {string} fileName name of the file, with .zip (relative to save path)
+  * @param {boolean} [includeFileName=false] if set to false, an array with all mods will be returned (mod is an object with name and version). If set to true, it will return an object with a name property and a mods property resolve({name: name_of_save, mods: mods})
+  * @returns {Promise.<Object>}
+  */
+  static getModsFromSaveFile(fileName, includeFileName = false) {
     return new Promise((resolve, reject) => {
       let file = path.join(this.savePath, fileName)
       jetpack.readAsync(file, 'buffer').then((buf) => {
@@ -409,7 +524,7 @@ class FactorioAPI {
             pos += length + 4
           }
 
-          if (resolveFileName) {
+          if (includeFileName) {
             resolve({name: fileName.replace('.zip', ''), mods: mods})
           } else {
             resolve(mods)
@@ -422,11 +537,21 @@ class FactorioAPI {
     })
   }
 
-  static getModsFromSave(saveName, resolveFileName = false) {
+  /**
+  * This function gets mods from a save file
+  * @param {string} saveName name of the save, without .zip (relative to save path)
+  * @param {boolean} [includeFileName=false] if set to false, an array with all mods will be returned (mod is an object with name and version). If set to true, it will return an object with a name property and a mods property resolve({name: name_of_save, mods: mods})
+  * @returns {Promise.<Object>}
+  */
+  static getModsFromSave(saveName, includeFileName = false) {
     let fileName = `${saveName}.zip`
-    return this.getModsFromSaveFile(fileName, resolveFileName)
+    return this.getModsFromSaveFile(fileName, includeFileName)
   }
 
+  /**
+  * This function gets mods from all save files
+  * @returns {Promise.<Object[]>} see docs for getModsFromSave for more info, includeFileName is always true (otherwise it would be confusing)
+  */
   static getModsFromSaves() {
     return new Promise((resolve, reject) => {
       jetpack.findAsync(this.savePath, { matching: '*.zip' }).then((files) => {
@@ -441,6 +566,11 @@ class FactorioAPI {
     })
   }
 
+  /**
+  * This function reads info.json from a zip file of a mod
+  * @param {string} fileName name of the mod, with .zip (relative to mod path)
+  * @returns {Promise.<Object>} returns parsed info.json file if resolved
+  */
   static readModZipFile(fileName) {
     return new Promise((resolve, reject) => {
       let file = path.join(this.modPath, fileName)
@@ -464,11 +594,22 @@ class FactorioAPI {
     })
   }
 
+  /**
+  * This function reads info.json from a zip file of a mod
+  * @param {Object} mod the mod you want to get info.json from
+  * @param {string} mod.name name of the mod
+  * @param {string} mod.version verison of the mod
+  * @returns {Promise.<Object>} returns parsed info.json file if resolved
+  */
   static readModZip(mod) {
     let fileName = `${mod.name}_${mod.version}.zip`
     return readModZipFile(fileName)
   }
 
+  /**
+  * This function reads info.json from all zip files in mod path
+  * @returns {Promise.<Object[]>} returns array of parsed info.json files (sorted alphabetically) if resolved
+  */
   static readModZips() {
     return new Promise((resolve, reject) => {
       jetpack.findAsync(this.modPath, { matching: '*_*.zip' }).then((files) => {
@@ -484,6 +625,10 @@ class FactorioAPI {
     })
   }
 
+  /**
+  * This function reads info.json from all zip files in mod path and applies information from mod-list.json to it (enabled or disabled)
+  * @returns {Promise.<Object[]>} returns array of parsed info.json files (sorted alphabetically) with enabled property if resolved
+  */
   static loadInstalledMods() {
     return new Promise((resolve, reject) => {
       this.readModZips().then((list) => {
@@ -511,6 +656,13 @@ class FactorioAPI {
     })
   }
 
+  /**
+  * This function saves the mod list
+  * @param {Object[]} mods all the installed mods
+  * @param {string} mods[].name name of the mod
+  * @param {string} mods[].enabled enabled property of the mod
+  * @returns {Promise}
+  */
   static saveModList(mods) {
     mods = mods.map(mod => {return {name: mod.name, enabled: mod.enabled.toString()}})
     return jetpack.writeAsync(path.join(this.modPath, 'mod-list.json'), JSON.stringify({mods: mods}))
